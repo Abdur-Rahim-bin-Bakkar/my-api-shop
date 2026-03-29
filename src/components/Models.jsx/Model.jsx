9cd7586ed23f3aef4d43b726df5e4d3862dbb78e
webdesignrahim4061 <webdesignrahim4061@gmail.com>
2026-03-29 11:56:58 +0600

    updated model sesction

--- src/components/Models.jsx/Model.jsx
+++ src/components/Models.jsx/Model.jsx
@@ -1,14 +1,19 @@
 import React, { use } from 'react';
 import ModeCard from './ModeCard';
 
-const Model = ({data, setAddAi, addAi}) => {
+const Model = ({ data, setAddAi, addAi }) => {
     const useData = use(data)
-    
+
     return (
-        <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-4 p-4 2xl:grid-cols-4'>
-            {
-                useData.map(model=> <ModeCard model={model} key={model.id} setAddAi={setAddAi} addAi={addAi}></ModeCard>)
-            }
+        <div className="">
+            <h1 className='mt-15 font-bold text-5xl text-center'>Choose Your AI Model</h1>
+            <p className='text-gray-400 text-center text-lg mt-5'>One subscription gives you access to all frontier AI models</p>
+            <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-4 p-4 2xl:grid-cols-4 mt-10'>
+                {
+                    useData.map(model => <ModeCard model={model} key={model.id} setAddAi={setAddAi} addAi={addAi}></ModeCard>)
+                }
+            </div>
+            <p className='text-center text-lg text-gray-500 my-16 text-sm'>All models are available under one powerful subscription • Cancel anytime</p>
         </div>
     );
 };
--- src/components/MyCards.jsx/MyCard.jsx
+++ src/components/MyCards.jsx/MyCard.jsx
@@ -1,23 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from './Card';
+import { ImFilesEmpty } from 'react-icons/im';
 
 const MyCard = ({addAi, setAddAi}) => {
       console.log(addAi)
+    //   const [totals, setTot] = useState(0)
+      const total = addAi.reduce((sum, ini)=>{
+        return sum + ini.price
+      },0)
     return (
         <div className='p-5'>
             <h1 className='font-bold  text-6xl my-5'>Your Card</h1>
             {
                addAi.length>0 &&  
                addAi.map(ai=>{
+                // setTot(totals + ai.price)
                 return(
+                    
                      <Card key={ai.id} data={ai} addAi={addAi} setAddAi={setAddAi} /> 
+                   
                 )
                })
             }
             {
+                addAi.length > 0 && 
+                <div className='space-y-4 mt-5'>
+                    <div className="card flex-row justify-between bg-black text-white font-bold p-6 m-5 rounded-2xl ">
+                        <h1 className='text-3xl'>Total</h1>
+                        <h1  className='text-3xl'>$ {total}</h1>
+                    </div>
+                    <div className="card  bg-[#e7000b] text-white font-bold p-6 m-5 rounded-2xl ">
+                        <h1 className='text-3xl text-center' onClick={()=> setAddAi([])}>Product to Checkout</h1>
+                        
+                    </div>
+                </div>
+            }
+            {
                 addAi.length === 0 &&
-                <div>
-                    <h1 className='font-bold text-center text-2xl my-25 text-gray-500'>Your cart is empty</h1>
+                <div className='mt-5'>
+                    <ImFilesEmpty className='text-7xl mx-auto m'></ImFilesEmpty>
+                    <h1 className='font-bold text-center text-2xl my-10 text-gray-500'>Your cart is empty</h1>
                 </div> 
             }
         </div>
